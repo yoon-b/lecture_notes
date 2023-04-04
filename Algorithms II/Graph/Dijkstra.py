38a2bdc282f8721c45abc1b42e80dbf8205f4909
yoon-b <eunbi.kim.kor@gmail.com>
2023-04-04 13:58:35 +0900

    add comments to Dijkstra

--- Algorithms II/Graph/Dijkstra.py
+++ Algorithms II/Graph/Dijkstra.py
@@ -14,19 +14,19 @@
 def dijkstra(s, V):
     U = [0] * (V+1)  # 최소비용이 결정된 정점 집합, like 'visited'
     U[s] = 1
-    for i in range(V+1):
+    for i in range(V+1):  # 시작점과 각 노드와의 거리를 업데이트
         D[i] = adjM[s][i]
 
-    for _ in range(V):
+    for _ in range(V):  # 시작점을 제외한 모든 노드에 대해.
         minV = inf
         w = 0
         for i in range(V+1):
-            if not U[i] and minV > D[i]:
+            if not U[i] and minV > D[i]:  # 아직 방문하지 않았고, 거리가 더 가까우면.
                 w = i
                 minV = D[i]
-        U[w] = 1
+        U[w] = 1  # 방문 기록 남기기
         for v in range(V+1):
-            if 0 < adjM[w][v] < inf:
+            if 0 < adjM[w][v] < inf:  # 자기자신이 아니고, 접근 가능하면.
                 D[v] = min(D[v], D[w]+adjM[w][v])
 
 
@@ -40,6 +40,6 @@ for _ in range(E):
     u, v, w = map(int, input().split())
     adjM[u][v] = w
 
-D = [0] * (V+1)
-dijkstra(0, V)
+D = [0] * (V+1)  # 해당 인덱스까지의 최단 거리를 담을 배열
+dijkstra(0, V)  # 시작점, 마지막 노드 번호
 print(D)
